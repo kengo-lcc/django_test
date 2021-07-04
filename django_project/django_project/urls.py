1cff6880042e8ea01227cafec69250b57ef24d7d
kengo <osada_kengo@lightcafe.co.jp>
2021-07-04 13:41:04 +0900

    update: newyearアプリを作成

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include("django_app.urls")),
+    path('newyear/', include("newyear.urls"))
 ]
