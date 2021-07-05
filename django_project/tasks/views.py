477779862d5ef642690c7907b1f59c02ad71396c
kengo <osada_kengo@lightcafe.co.jp>
2021-07-05 13:31:52 +0900

    update: tasksアプリでsessionを使えるように修正

--- django_project/tasks/views.py
+++ django_project/tasks/views.py
@@ -3,41 +3,29 @@ from django import forms
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 
-tasks = ["foo", "bar", "baz"]
 # Create your views here.
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
+    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
 
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 # Add a new task:
 def add(request):
 
-    # check if method is POST
     if request.method == "POST":
-
-        # Take in the data the user submitted and save it as form
         form = NewTaskForm(request.POST)
-
-        # Check if form data is valid (server-side)
         if form.is_valid():
-
-            # Isolate the task from the 'cleaned' version of form data
             task = form.cleaned_data["task"]
-
-            # Add the new task to our list of tasks
-            tasks.append(task)
-
-            # Redirect user to list of tasks
+            request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
-
         else:
-
-            # If the form is invalid, re-render the page with existing information.
             return render(request, "tasks/add.html", {
                 "form": form
             })
